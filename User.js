f8ee198acf580534500e4d11228d769625e8bb84
deanosagiede252-cloud <deanosagiede252@gmail.com>
2025-11-06 13:30:38 +0100

    Delete User.js

--- User.js
+++
@@ -1,1 +0,0 @@
-import mongoose from 'mongoose'; const UserSchema = new mongoose.Schema({ email:{type:String,required:true,unique:true}, passwordHash:{type:String,required:true}, name:String, role:{type:String,default:'user'} }); export default mongoose.models.User || mongoose.model('User', UserSchema);
